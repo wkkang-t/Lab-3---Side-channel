196ef32db96e7d395fb4c654e154d395414734f1
minxuan <minxuan_chang@mymail.sutd.edu.sg>
2022-08-10 17:14:36 +0800

    timehack v1

--- timehack/timehack.c
+++ timehack/timehack.c
@@ -10,7 +10,8 @@
 
 int main(int argc, char **argv) {
     char guess[33];
-
+    long a,d, f, g;
+    long timer[94];
     // set guess to zeros
     bzero(guess, sizeof(guess));
 
@@ -20,11 +21,49 @@ int main(int argc, char **argv) {
     //   password, each time trying all possible characters
     //   and looking for time deviations
     //
-    if (check_pass(guess)) {
-        printf("Password Found!\n");
-        hack_system(guess);
-    };
+    a = rdtsc();
+    check_pass(guess);
+    //char c = 73;
+    //guess[0]=c;
+    //c = 115;
+    //guess [1] = 115;
 
+    for (char c = 33; c < 127; c++) {
+        a = rdtsc();
+        check_pass(guess);
+        d = rdtsc();
+        timer[c-33] = d-a;
+    }
+
+    for (int i=1; i <= 92;  i++) {
+        for (int j=0; i<= 93-i;j++) {
+            if (timer[j] <= timer[j+1]){
+                long t = timer[j];
+                timer[j] = timer[j+1];
+                timer[j+1] = t;
+            } else continue;
+        }
+    }
+
+    long median = (timer[94/2] + timer[94/2+1])/2;
+
+    for (int i=0;i<12;i++){
+    for (char c = 33; c<127; c++){
+        guess[i] =c;
+	a = rdtsc();
+	check_pass(guess);	
+    //if (check_pass(guess)) {
+    //    printf("Password Found!\n");
+    //    hack_system(guess);
+    //};
+	d = rdtsc();
+	
+    if ((d-a)/i+1 > median + 100) {
+        printf("%s", guess);
+        break;
+    }
+    }
+    }
     printf("Could not get the password!  Last guess was %s\n", guess);
     return 1;
 };
